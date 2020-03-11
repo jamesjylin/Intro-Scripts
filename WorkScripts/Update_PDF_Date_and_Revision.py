8f2c0ed4e2c8fc416f058f95ca7acc9923e21c2e
James Lin <jamesjy.lin2000@gmail.com>
2020-03-11 11:59:26 -0400

    Add files via upload

---
+++ WorkScripts/Update_PDF_Date_and_Revision.py
@@ -0,0 +1,37 @@
+import os
+
+# Change revision, date, and directory, and run the program, leave everything else
+# Function to rename multiple files
+def main():
+
+    # Copy the location of the files in the quotations. Make sure to include double backspaces.
+    # E.g. "C:\\Users\\pass.ecs.coop\\Desktop"
+    directory = "C:\\Users\\pass.ecs.coop\\Desktop\\Test Files"
+
+    # Revision number: if revision is PG2, set revision = "2"
+    revision = "2"
+
+    # Date: set to desired date format within quotations
+    date = "MAR 10, 2020"
+
+    os.chdir(directory)
+
+    # Loop through directory
+    for filename in os.listdir():
+
+        # Finds length of file name, removes ending (PG# DATE) to be replaced
+        i = 0
+        for i in range(0, len(filename)):
+            if filename[i] == "(":
+                break
+
+        # Renames files
+        src = filename
+        dst = filename[:i] + "(PG" + revision + " " + date + ").pdf"
+        os.rename(src, dst)
+
+
+# Driver Code
+if __name__ == '__main__':
+    # Calling main() function
+    main()
